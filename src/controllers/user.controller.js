4388d1535d9a13f12f879d9c636a9f47af39df72
Vivek Prasad <flashtech007@gmail.com>
2024-01-15 13:39:14 +0530

    completed testing for register user controller 1. bug fixes 2. testing on postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,16 +16,14 @@ const registerUser = asyncHandler(async(req, res) => {
     // check for user creation
     // return res
 
-
     const {fullName, email, username, password } = req.body
     console.log(email)
-    res.send("")
 
     if([fullName, email, username, password].some((field) => (field?.trim() === ""))){
         throw new ApiError(400, "All fields are required")
     }
 
-    const userExists = User.findOne({
+    const userExists = await User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -35,7 +33,12 @@ const registerUser = asyncHandler(async(req, res) => {
 
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
@@ -57,17 +60,19 @@ const registerUser = asyncHandler(async(req, res) => {
         username: username.toLowerCase(),
     })
 
-    const createdUser = User.findById(newUser._id).select(
+    const createdUser = await User.findById(newUser._id).select(
         "-password -refreshToken"
     )
 
 
     if(!createdUser){
-        throw new ApiError(500, "Something went wrong")
+        throw new ApiError(500, "Something went wrong while registering the user")
     }
 
+       
+
     return res.status(201).json(
-        new ApiResponse(200, createdUser, "User Registered Successfully")
+        new ApiResponse(200, createdUser, "User Registered Successfullyy")
     )
 
 })
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,9 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
 
         // file has been upload successfully
-        console.log("file is uploaded on cloudinary ", response.url)
+
+        // console.log("file is uploaded on cloudinary ", response.url)
+        fs.unlinkSync(localFilePath)
         return response;
 
     } catch(err){
@@ -27,9 +29,9 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
 
 
   export {uploadOnCloudinary}
