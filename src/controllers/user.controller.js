41e03a6c55728b6fc5b458eec91a16b329c5b574
Vivek Prasad <flashtech007@gmail.com>
2024-01-16 13:53:58 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -197,7 +197,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     try {
         const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
     
-        if(incomingRefreshToken){
+        if(!incomingRefreshToken){
             throw new ApiError(401, "unauthorized request")
         }
     
